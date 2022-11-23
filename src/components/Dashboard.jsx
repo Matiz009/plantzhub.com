c10b5186582fdc4f295a30fd71fbff09d6c76a36
MATI UL REHMAN <iammatiz00@gmail.com>
2022-11-23 18:56:25 +0500

    dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,20 +6,19 @@ const Dashboard = () => {
   return (
     <div>
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand style={{ marginLeft: "20px" }}>plantzHub</Navbar.Brand>
+        <Navbar.Brand style={{ marginLeft: "20px" }}>
+          plantzHub.com
+        </Navbar.Brand>
         <Nav.Link className="text-white" style={{ paddingLeft: "30%" }}>
           <h1>Admin Dashboard</h1>
         </Nav.Link>
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>
-            Signed in as: <a className="text-decoration-none">Mati ul Rehman</a>
+            Signed in as: <p className="text-decoration-none">Mati ul Rehman</p>
           </Navbar.Text>
         </Navbar.Collapse>
       </Navbar>
-      <footer style={{ height: "100px" }} className="bg-success">
-        <br />
-        <h5 className="text-dark">Copyrights Reserved 2022</h5>
-      </footer>
+      <button className="btn btn-dark">Add Plants</button>
     </div>
   );
 };
